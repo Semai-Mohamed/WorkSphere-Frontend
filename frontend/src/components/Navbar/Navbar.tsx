017369970ce833fd1e7d26dd8f4783073a52a72c
hamza785 <hamza.dje785@gmail.com>
2025-08-18 18:19:50 +0100

    moved user-menu into a seperate component

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 "use client";
 import Image from "next/image";
-import MenuButton from "./MenuButton";
+import MenuButton from "../MenuButton";
 import { useEffect, useMemo, useRef, useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import FilterBar from "./FilterBar";
+import UserMenu from "./UserMenu";
 
 export default function Navbar() {
     const path = usePathname();
@@ -73,40 +74,10 @@ export default function Navbar() {
                             </div>
                         </div>
 
-                        <div
-                            className={`absolute top-[80px] lg:right-[50%] max-lg:right-0 lg:translate-x-[50%] w-[360px] grid grid-cols-2 gap-x-2.5 max-sm:pl-12 max-sm:w-screen ${userMenuShown
-                                ? "gap-y-2.5"
-                                : "gap-y-0 pointer-events-none -translate-y-5 opacity-0"
-                                } transition-all duration-200 ease-out`}
-                        >
-                            <MenuButton name="Dashboard" />
-                            <MenuButton name="Working On" />
-                            <MenuButton name="My Services" href="services" />
-                            <MenuButton name="Withdraw" />
-                            <MenuButton name="Profile" />
-                            <MenuButton name="Account" />
-                            <button className="normal-button col-span-2 my-2">
-                                Post a service
-                            </button>
-                            <MenuButton
-                                name="Get Support"
-                                className="border-[oklch(from_var(--color-blue)_l_c_h_/_0.15)]! text-blue! hover:shadow-[0_4px_14px_oklch(from_var(--color-blue)_l_c_h_/_0.2)]! hover:border-white! focus:bg-blue! focus:text-white!"
-                                imageClassName="bg-blue! group-focus:bg-white!"
-                                href="support"
-                            />
-                            <MenuButton
-                                name="Log Out"
-                                className="border-[oklch(from_var(--color-alternative-red)_l_c_h_/_0.15)]! text-alternative-red! hover:shadow-[0_4px_14px_oklch(from_var(--color-alternative-red)_l_c_h_/_0.2)]! hover:border-white! focus:bg-alternative-red! focus:text-white!"
-                                imageClassName="bg-alternative-red! group-focus:bg-white!"
-                                href="logout"
-                            />
-                            <button
-                                onClick={() => setUserMenuShown(u => !u)}
-                                className="font-primary text-[11px] opacity-20 cursor-pointer hover:opacity-100 px-2 py-1 absolute right-1 -bottom-8 duration-200 transition-opacity"
-                            >
-                                Close
-                            </button>
-                        </div>
+                        <UserMenu
+                            userMenuShown={userMenuShown}
+                            setUserMenuShown={setUserMenuShown}
+                        />
                     </div>
                 </div>
             </header>
